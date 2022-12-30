d5d53bd772b5949c7af1ea52e21b11fe529c4c52
MaiNguyendz <118418729+MaiNguyendz@users.noreply.github.com>
2022-12-30 20:13:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Có điều này mún nói với Phúc nè`,
+    btnIntro: 'Oke',
+    title: 'Cho Nguyên rủ Phúc mai đi chơi nhe',
+    desc: 'Phúc khum trả lời mà thóat ra tức là đồng ý đó nghe ',
+    btnYes: 'O tê lun :"))',
+    btnNo: 'À khum mai t mắc ròi ',
+    question:'Ây dà biết ngay sẽ o tê mà, z định đi đâu đây',
+    reasonPlaceholder: 'Địa điểm',
+    btnReply: 'Gởi ! ',
+    mess: 'O tê nhe go go',
+    messDesc: 'Qua fb nt i :")',
+    btnAccept: 'Oke lun',
+    messLink: 'https://mainguyendz.github.io/........./'
+}
